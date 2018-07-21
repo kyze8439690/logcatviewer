8a9197cb476ff00b47e7ce426676a156a953a08c
YangHui <me@yanghui.name>
2018-07-21 17:52:14 +0800

    fix pattern error

--- src/main/java/com/github/logviewer/LogItem.java
+++ src/main/java/com/github/logviewer/LogItem.java
@@ -23,7 +23,7 @@ public class LogItem implements Parcelable {
     private static final String PRIORITY_FATAL = "F";
 
     private static final Pattern sLogcatPattern = Pattern.compile(
-            "([0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}\\.[0-9]{3})[ ]+([0-9]+)[ ]+([0-9]+) ([VDIWEF]) (.*): (.*)");
+              "([0-9^-]+-[0-9^-]+ [0-9^:]+:[0-9^:]+\\.[0-9]+) ([0-9]+) ([0-9]+) ([VDIWEF]) ([^:]+): (.*)");
     private static final SimpleDateFormat sDataFormat = new SimpleDateFormat(
             "MM-dd hh:mm:ss.SSS", Locale.getDefault());
 
