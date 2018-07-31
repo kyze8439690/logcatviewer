84808f12c6be73035ff54650b202fe979cb80a6b
YangHui <me@yanghui.name>
2018-07-31 22:52:35 +0800

    fix compatibility issue

--- src/main/java/com/github/logviewer/LogItem.java
+++ src/main/java/com/github/logviewer/LogItem.java
@@ -23,7 +23,7 @@ public class LogItem implements Parcelable {
     private static final String PRIORITY_FATAL = "F";
 
     private static final Pattern sLogcatPattern = Pattern.compile(
-              "([0-9^-]+-[0-9^-]+ [0-9^:]+:[0-9^:]+\\.[0-9]+) ([0-9]+) ([0-9]+) ([VDIWEF]) ([^:]+): (.*)");
+              "([0-9^-]+-[0-9^ ]+ [0-9^:]+:[0-9^:]+\\.[0-9]+) +([0-9]+) +([0-9]+) ([VDIWEF]) ([^:]+): (.*)");
     private static final SimpleDateFormat sDataFormat = new SimpleDateFormat(
             "MM-dd hh:mm:ss.SSS", Locale.getDefault());
 
@@ -48,6 +48,7 @@ public class LogItem implements Parcelable {
     static final ArrayList<String> IGNORED_LOG = new ArrayList<String>() {{
         add("--------- beginning of crash");
         add("--------- beginning of main");
+        add("--------- beginning of system");
     }};
 
     public Date time;
