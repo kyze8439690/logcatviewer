b8f11f945b8aabfb16e52c504aec7900706b5c67
kyze8439690 <me@yanghui.name>
2019-10-24 00:06:39 +0800

    improve logcat compatibility

--- src/main/java/com/github/logviewer/LogItem.java
+++ src/main/java/com/github/logviewer/LogItem.java
@@ -23,7 +23,7 @@ public class LogItem implements Parcelable {
     private static final String PRIORITY_FATAL = "F";
 
     private static final Pattern sLogcatPattern = Pattern.compile(
-              "([0-9^-]+-[0-9^ ]+ [0-9^:]+:[0-9^:]+\\.[0-9]+) +([0-9]+) +([0-9]+) ([VDIWEF]) ([^:]*): (.*)");
+              "([0-9^-]+-[0-9^ ]+ [0-9^:]+:[0-9^:]+\\.[0-9]+) +([0-9]+) +([0-9]+) ([VDIWEF]) ((?!: ).)+: (.*)");
 
     private static final HashMap<String, Integer> LOGCAT_COLORS = new HashMap<String, Integer>() {{
         put(PRIORITY_VERBOSE, R.color.logcat_verbose);
