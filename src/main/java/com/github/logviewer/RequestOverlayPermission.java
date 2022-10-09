17afca16b078688ff5d686e4b6585c70ab0e5c5b
kyze8439690 <me@yanghui.name>
2022-10-10 00:11:07 +0800

    adapt to registerForActivityResult api

---
+++ src/main/java/com/github/logviewer/RequestOverlayPermission.java
@@ -0,0 +1,67 @@
+package com.github.logviewer;
+
+import static android.os.Build.VERSION_CODES.M;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.net.Uri;
+import android.os.Build.VERSION;
+import android.provider.Settings;
+import android.widget.Toast;
+
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.annotation.NonNull;
+
+import org.jetbrains.annotations.Nullable;
+
+
+public class RequestOverlayPermission extends ActivityResultContract<Void, Boolean> {
+
+    private final Context mContext;
+
+    public RequestOverlayPermission(Context context) {
+        mContext = context;
+    }
+
+    private Intent getIntent(Context context) {
+        if (VERSION.SDK_INT >= M) {
+            return new Intent(
+                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + context.getPackageName())
+            );
+        } else {
+            throw new IllegalStateException();
+        }
+    }
+
+    @NonNull
+    @Override
+    public Intent createIntent(@NonNull Context context, Void unused) {
+        return getIntent(context);
+    }
+
+    @Override
+    public Boolean parseResult(int i, @Nullable Intent intent) {
+        return VERSION.SDK_INT >= M && Settings.canDrawOverlays(mContext);
+    }
+
+    @Nullable
+    @Override
+    public SynchronousResult<Boolean> getSynchronousResult(@NonNull Context context, Void input) {
+        if (VERSION.SDK_INT < M || Settings.canDrawOverlays(context)) {
+            return new SynchronousResult<>(true);
+        }
+        if (context.getPackageManager().queryIntentActivities(getIntent(context), 0).isEmpty()) {
+            return new SynchronousResult<>(false);
+        } else {
+            ApplicationInfo applicationInfo = context.getApplicationInfo();
+            int stringId = applicationInfo.labelRes;
+            String appName = stringId == 0 ? applicationInfo.nonLocalizedLabel.toString() :
+                    context.getString(stringId);
+            String toast = context.getString(R.string.logcat_viewer_grant_permission_hint, appName);
+            Toast.makeText(context, toast, Toast.LENGTH_SHORT).show();
+            return null;
+        }
+    }
+}
