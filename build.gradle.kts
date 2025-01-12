9653399b3720f65859507ea137bc124eb7abfed0
kyze8439690 <me@yanghui.name>
2025-01-12 15:35:04 +0800

    adapt to android 15 edgeToEdge

---
+++ build.gradle.kts
@@ -0,0 +1,53 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        //noinspection UseTomlInstead
+        classpath("com.android.tools.build:gradle:8.7.3")
+    }
+}
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    buildFeatures {
+        dataBinding = false
+        viewBinding = true
+    }
+
+    compileSdk = 35
+    buildToolsVersion = "35.0.0"
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+    namespace = "com.github.logviewer"
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    //noinspection UseTomlInstead
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.activity:activity-ktx:1.9.3")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+}
