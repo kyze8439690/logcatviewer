92bc84d33a83cd73723f15da167303084d3f0d7a
kyze8439690 <yugy1991@gmail.com>
2026-08-03 00:43:42 +0800

    #832 - chore: remove redundant jvmTarget override
    Built-in Kotlin defaults jvmTarget to android.compileOptions.targetCompatibility
    (Java 21), making the explicit kotlin.compilerOptions block unnecessary.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 buildscript {
     repositories {
         google()
@@ -44,11 +42,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
-    }
-    kotlin {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_21)
-        }
     }
 }
 
